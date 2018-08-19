593793a9f90235a7d4dd2b02574f90b5871af20c
anthony <anthony@anthonymurphy.me>
2018-08-19 23:36:02 +1000

    Remove import with .*

--- src/test/java/me/anthonymurphy/ratelimiter/TokenBucketImplTest.java
+++ src/test/java/me/anthonymurphy/ratelimiter/TokenBucketImplTest.java
@@ -8,7 +8,10 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
